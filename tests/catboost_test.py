eeec0ae7f338fc6b3233d53c12a0e31c49adf9bc
xdssio <jonathan@xdss.io>
2021-11-25 14:30:43 +0100

    pip8

--- tests/catboost_test.py
+++ tests/catboost_test.py
@@ -12,22 +12,24 @@ from goldilox.vaex.pipeline import VaexPipeline as VaexPipeline
 @pytest.fixture()
 def iris():
     # iris = load_iris_1e5()
-
     return load_iris_1e5()
 
 
 def test_vaex_catboost(iris):
     train, test = iris.ml.train_test_split(test_size=0.2, verbose=False)
-    features = ['petal_length', 'petal_width', 'sepal_length', 'sepal_width']
-    target = 'class_'
-    train['X'] = train['petal_length'] / train['petal_width']
+    features = ["petal_length", "petal_width", "sepal_length", "sepal_width"]
+    target = "class_"
+    train["X"] = train["petal_length"] / train["petal_width"]
     from vaex.ml.catboost import CatBoostModel
-    booster = CatBoostModel(features=features,
-                            target=target,
-                            task_type="GPU",
-                            prediction_name='predictions',
-                            num_boost_round=500, params={'verbose': 0, 'iterations': 10,
-                                                         'objective': 'MultiClass'})
+
+    booster = CatBoostModel(
+        features=features,
+        target=target,
+        task_type="GPU",
+        prediction_name="predictions",
+        num_boost_round=500,
+        params={"verbose": 0, "iterations": 10, "objective": "MultiClass"},
+    )
     booster.fit(train)
     train = booster.transform(train)
 
@@ -35,37 +37,49 @@ def test_vaex_catboost(iris):
     def argmax(ar, axis=1):
         return np.argmax(ar, axis=axis)
 
-    train.add_function('argmax', argmax)
-    train['prediction'] = train['predictions'].argmax()
+    train.add_function("argmax", argmax)
+    train["prediction"] = train["predictions"].argmax()
 
     pipeline = VaexPipeline.from_dataframe(train)
-    pipeline.set_variable('accuracy',
-                          accuracy_score(pipeline.inference(test[features])['prediction'].values, test[target].values))
+    pipeline.set_variable(
+        "accuracy",
+        accuracy_score(
+            pipeline.inference(test[features])["prediction"].values, test[target].values
+        ),
+    )
 
     sample = test.to_records(0)
 
     assert pipeline.inference(test).head(1).shape == (1, 8)
     assert pipeline.inference(sample).head(1).shape == (1, 8)
-    assert pipeline.inference({'sepal_length': 5.9, 'petal_length': 4.2}).head(1).shape == (1, 8)
-    assert pipeline.inference({'sepal_length': 5.9, 'petal_length': 4.2}, columns='prediction').head(1).shape == (1, 1)
-    assert pipeline.inference({'sepal_length': 5.9, 'petal_length': 4.2, 'Y': 'new column'}).head(1).shape == (1, 9)
+    assert pipeline.inference({"sepal_length": 5.9, "petal_length": 4.2}).head(
+        1
+    ).shape == (1, 8)
+    assert pipeline.inference(
+        {"sepal_length": 5.9, "petal_length": 4.2}, columns="prediction"
+    ).head(1).shape == (1, 1)
+    assert pipeline.inference(
+        {"sepal_length": 5.9, "petal_length": 4.2, "Y": "new column"}
+    ).head(1).shape == (1, 9)
 
 
 def test_catboost_vaex_fit(iris, tmpdir):
-
     def fit(df):
         from sklearn.metrics import accuracy_score
         from vaex.ml.catboost import CatBoostModel
+
         train, test = df.ml.train_test_split(test_size=0.2, verbose=False)
 
-        features = ['petal_length', 'petal_width', 'sepal_length', 'sepal_width']
-        target = 'class_'
+        features = ["petal_length", "petal_width", "sepal_length", "sepal_width"]
+        target = "class_"
 
-        booster = CatBoostModel(features=features,
-                                target=target,
-                                prediction_name='predictions',
-                                num_boost_round=500, params={'verbose': 0, 'iterations': 10,
-                                                             'objective': 'MultiClass'})
+        booster = CatBoostModel(
+            features=features,
+            target=target,
+            prediction_name="predictions",
+            num_boost_round=500,
+            params={"verbose": 0, "iterations": 10, "objective": "MultiClass"},
+        )
         booster.fit(train)
         train = booster.transform(train)
 
@@ -73,44 +87,57 @@ def test_catboost_vaex_fit(iris, tmpdir):
         def argmax(ar, axis=1):
             return np.argmax(ar, axis=axis)
 
-        train.add_function('argmax', argmax)
-        train['prediction'] = train['predictions'].argmax()
+        train.add_function("argmax", argmax)
+        train["prediction"] = train["predictions"].argmax()
         pipeline = VaexPipeline.from_dataframe(train)
-        accuracy = accuracy_score(pipeline.inference(test[features])['prediction'].values,
-                                  test[target].values)
-        booster = CatBoostModel(features=features,
-                                target=target,
-                                prediction_name='predictions',
-                                num_boost_round=500, params={'verbose': 0, 'iterations': 10,
-                                                             'objective': 'MultiClass'})
+        accuracy = accuracy_score(
+            pipeline.inference(test[features])["prediction"].values, test[target].values
+        )
+        booster = CatBoostModel(
+            features=features,
+            target=target,
+            prediction_name="predictions",
+            num_boost_round=500,
+            params={"verbose": 0, "iterations": 10, "objective": "MultiClass"},
+        )
         booster.fit(df)
         df = booster.transform(df)
-        df.variables['accuracy'] = accuracy
-        df.add_function('argmax', argmax)
-        df['prediction'] = df['predictions'].argmax()
+        df.variables["accuracy"] = accuracy
+        df.add_function("argmax", argmax)
+        df["prediction"] = df["predictions"].argmax()
         return df
 
     df = iris.copy()
     pipeline = VaexPipeline.from_dataframe(df, fit=fit)
     data = df.to_records(0)
-    data.pop('class_')
+    data.pop("class_")
     assert pipeline.inference(data).shape == df.head(1).shape
     data = df.to_records(0)
     pipeline.fit(df)
     pipeline = validate_persistance(pipeline)
     assert pipeline.inference(data).shape == (1, 7)
-    assert pipeline.get_variable('accuracy')
+    assert pipeline.get_variable("accuracy")
     assert pipeline.raw == data
-    assert list(pipeline.example.keys()) == ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class_',
-                                             'predictions', 'prediction']
+    assert list(pipeline.example.keys()) == [
+        "sepal_length",
+        "sepal_width",
+        "petal_length",
+        "petal_width",
+        "class_",
+        "predictions",
+        "prediction",
+    ]
 
 
 def test_catboost_sklearn(iris):
     from catboost import CatBoostClassifier
+
     df = iris.copy().to_pandas_df()
-    features = ['petal_length', 'petal_width', 'sepal_length', 'sepal_width']
-    target = 'class_'
-    sk_pipeline = sklearn.pipeline.Pipeline([('classifier', CatBoostClassifier(verbose=0, iterations=10))])
+    features = ["petal_length", "petal_width", "sepal_length", "sepal_width"]
+    target = "class_"
+    sk_pipeline = sklearn.pipeline.Pipeline(
+        [("classifier", CatBoostClassifier(verbose=0, iterations=10))]
+    )
     X = df[features]
     y = df[target]
     self = pipeline = SklearnPipeline.from_sklearn(sk_pipeline).fit(X, y)
@@ -119,9 +146,11 @@ def test_catboost_sklearn(iris):
     assert pipeline.inference(self.raw).shape == (1, 5)
 
     df = iris.copy()
-    features = ['petal_length', 'petal_width', 'sepal_length', 'sepal_width']
-    target = 'class_'
-    sk_pipeline = sklearn.pipeline.Pipeline([('classifier', CatBoostClassifier(verbose=0, iterations=10))])
+    features = ["petal_length", "petal_width", "sepal_length", "sepal_width"]
+    target = "class_"
+    sk_pipeline = sklearn.pipeline.Pipeline(
+        [("classifier", CatBoostClassifier(verbose=0, iterations=10))]
+    )
     X = df[features]
     y = df[target]
     self = pipeline = SklearnPipeline.from_sklearn(sk_pipeline).fit(X, y)
