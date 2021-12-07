b5c214f0f61f251fd0cf20f8c5e33a702229dac0
Yonatan Alexander <yonatanalexander@ip-192-168-2-181.eu-central-1.compute.internal>
2021-12-07 18:20:57 +0100

    missing requierment to run server

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="goldilox",
     packages=find_packages(),
-    install_requires=["sklearn", "pandas", "numpy", "cloudpickle"],
+    install_requires=["sklearn", "pandas", "numpy", "cloudpickle", "gunicorn"],
     version="0.0.1a4",
     url="https://github.com/xdssio/goldilox",
     description="A tool for deploying machine learning",
