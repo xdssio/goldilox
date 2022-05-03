0fd256497d71c34b2b9e2621668021bfcdea6226
xdssio <jonathan@xdss.io>
2022-05-03 17:33:13 +0200

    add annotation signature hints

--- goldilox/config.py
+++ goldilox/config.py
@@ -3,37 +3,41 @@ from distutils.util import strtobool
 
 AWS_PROFILE = os.environ.get("AWS_PROFILE")
 
+
 # Constants
-STATE = "state"
-CREATED = "created"
-UPDATED = "updated"
-NO_SELECTIONS = {"__filter__": None}
-CONSTANTS = [str, int, float, bool]
-COLUMNS = "columns"
-VIRTUAL = "virtual"
-OUTPUT_TYPE = "output_type"
-NOT_IMPLEMENTED_ERROR = "Not implemented"
-JSON = "json"
-PANDAS = "pandas"
-VAEX = "vaex"
-SKLEARN = "sklearn"
-NUMPY = "numpy"
-SELECTIONS = "selections"
-SELECTIONS_FILTER = "__filter__"
-VARIABLES = "variables"
-DESCRIPTION = "description"
-RAW = "raw"
-PIPELINE = "pipeline"
-PIPELINE_TYPE = "pipeline_type"
-STATE = "state"
-VERSION = "version"
-PY_VERSION = "py_version"
-REQUIREMEMTS = "requirements"
-CONDA_ENV = "conda_env"
+class CONSTANTS:
+    STATE = "state"
+    CREATED = "created"
+    UPDATED = "updated"
+    NO_SELECTIONS = {"__filter__": None}
+    CONSTANTS = [str, int, float, bool]
+    COLUMNS = "columns"
+    VIRTUAL = "virtual"
+    OUTPUT_TYPE = "output_type"
+    NOT_IMPLEMENTED_ERROR = "Not implemented"
+    JSON = "json"
+    PANDAS = "pandas"
+    VAEX = "vaex"
+    SKLEARN = "sklearn"
+    NUMPY = "numpy"
+    SELECTIONS = "selections"
+    SELECTIONS_FILTER = "__filter__"
+    VARIABLES = "variables"
+    DESCRIPTION = "description"
+    RAW = "raw"
+    PIPELINE = "pipeline"
+    PIPELINE_TYPE = "pipeline_type"
+    STATE = "state"
+    VERSION = "version"
+    PY_VERSION = "py_version"
+    REQUIREMEMTS = "requirements"
+    CONDA_ENV = "conda_env"
+    DEFAULT_SUFFIX = ".parquet"
+    BYTE_DELIMITER = b'###'
+    VENV_TYPE = 'venv_type'
+
+
 VALID_VAEX_HEADERS = ["*.csv", "*.hdf5", "*.parquet", "*.arrow"]
-DEFAULT_SUFFIX = ".parquet"
-BYTE_DELIMITER = b'###'
-VENV_TYPE = 'venv_type'
 
 # App
 CORS_ORIGINS = os.getenv('ORIGINS', '*').split(',')
