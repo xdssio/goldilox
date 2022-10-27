02632b5131c598546745445a707f507cb036e5f4
xdssio <jonathan@xdss.io>
2022-10-27 17:22:46 +0200

    fix app tests
    * fix app tests
    * add locust manual testing

--- tests/app_test.py
+++ tests/app_test.py
@@ -2,7 +2,6 @@ import io
 from tempfile import TemporaryDirectory
 
 import pandas as pd
-import pytest
 import vaex
 from fastapi.testclient import TestClient
 from sklearn.linear_model import LogisticRegression
@@ -11,6 +10,8 @@ from vaex.ml.sklearn import Predictor
 import goldilox
 import goldilox.app
 from goldilox.datasets import load_iris
+
+app_pipeline = goldilox.app.AppPipeline()
 
 
 def _pipeline():
@@ -28,84 +29,81 @@ def _pipeline():
     return pipeline
 
 
-def _client():
-    pipeline = _pipeline()
-    model_path = str(TemporaryDirectory().name) + '/pipeline.pkl'
-    pipeline.save(model_path)
-    return TestClient(goldilox.app.get_app(model_path))
+pipeline = _pipeline()
+model_path = str(TemporaryDirectory().name) + '/pipeline.pkl'
+pipeline.save(model_path)
+
+app = goldilox.app.get_app(model_path)
+raw = {'sepal_length': 5.1, 'sepal_width': 3.5, 'petal_length': 1.4, 'petal_width': 0.2}
 
 
-def _raw():
-    return {'sepal_length': 5.1, 'sepal_width': 3.5, 'petal_length': 1.4, 'petal_width': 0.2}
+@app.on_event('startup')
+async def load_model():
+    app_pipeline.pipeline = pipeline
+    app_pipeline.meta = app_pipeline.pipeline.meta
+    app_pipeline.pipeline.example  # warmup and load packages
 
 
-@pytest.fixture()
-def raw():
-    return _raw()
-
-
-@pytest.fixture()
-def client():
-    return _client()
-
-
-def test_app_variables(client):
-    response = client.get('/variables').json()
+def test_app_variables():
+    with TestClient(app) as client:
+        response = client.get('/variables').json()
     assert response['a'] == 1
     assert response['b'] == 2
     assert response['description'] == 'description'
     assert response['variables']['test'] == 'test'
 
 
-def test_app_description(client):
-    assert client.get('/description').json() == 'description'
+def test_app_description():
+    with TestClient(app) as client:
+        assert client.get('/description').json() == 'description'
 
 
-def test_app_inference(client, raw):
-    predictions = client.post('/inference?columns=prediction,target', json=[raw, raw]).json()
+def test_app_inference():
+    with TestClient(app) as client:
+        predictions = client.post('/inference?columns=prediction,target', json=[raw, raw]).json()
     for prediction in predictions:
         assert prediction['prediction'] == 0
         assert prediction['target'] is None
 
 
-def test_app_invocations(client, raw):
-    client = _client()
-    raw = _raw()
-    pipeline = _pipeline()
-
+def test_app_invocations():
     # standard
     payload = [raw, raw]
-    predictions = client.post('/invocations', json=payload).json()
+    with TestClient(app) as client:
+        predictions = client.post('/invocations', json=payload).json()
     for prediction in predictions:
         assert prediction['prediction'] == 0
         assert prediction['target'] is None
 
     # mlflow format
     payload = {"data": [list(raw.values()), list(raw.values())]}
-    predictions = client.post('/invocations?columns=prediction', json=payload).json()
+    with TestClient(app) as client:
+        predictions = client.post('/invocations?columns=prediction', json=payload).json()
     assert predictions[0]['prediction'] == 0
     df = pd.DataFrame([raw, raw])
     buffer = io.StringIO()
     df.to_csv(buffer, index=False, header=True)
 
     # csv format
-    response = client.post('/invocations', headers={'Content-Type': 'text/csv'}, data=buffer.getvalue()).json()
+    with TestClient(app) as client:
+        response = client.post('/invocations', headers={'Content-Type': 'text/csv'}, data=buffer.getvalue()).json()
     predictions = pd.read_csv(io.StringIO(response))
     assert predictions.shape[1] == df.shape[1] + 2
-
-    response = client.post('/invocations?columns=target,prediction', headers={'Content-Type': 'text/csv'},
-                           data=buffer.getvalue()).json()
+    with TestClient(app) as client:
+        response = client.post('/invocations?columns=target,prediction', headers={'Content-Type': 'text/csv'},
+                               data=buffer.getvalue()).json()
     predictions = pd.read_csv(io.StringIO(response))
     assert predictions.shape[1] == 2
 
 
-def test_app_ping(client):
-    client = _client()
-    assert client.get('/ping').json() == 'pong'
+def test_app_ping():
+    with TestClient(app) as client:
+        assert client.get('/ping').json() == 'pong'
 
 
-def test_app_example(client):
-    response = client.get('/example').json()[0]
+def test_app_example():
+    with TestClient(app) as client:
+        response = client.get('/example').json()[0]
     assert response['sepal_length'] == 5.1
     assert response['sepal_width'] == 3.5
     assert response['petal_length'] == 1.4
---
+++ tests/locust_test_manually.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, TaskSet, task, between
+
+
+class IrisPredict(TaskSet):
+    @task
+    def predict(self):
+        request_body = [
+            {
+                "sepal_length": 5.1,
+                "sepal_width": 3.5,
+                "petal_length": 1.4,
+                "petal_width": 0.2
+            }
+        ]
+        self.client.post('/inference', json=request_body)
+
+
+class IrisLoadTest(HttpUser):
+    tasks = [IrisPredict]
+    host = 'http://127.0.0.1:5000'
+    stop_timeout = 20
+    wait_time = between(1, 5)
+
+# locust -f ./tests/locust_test_manually.py
